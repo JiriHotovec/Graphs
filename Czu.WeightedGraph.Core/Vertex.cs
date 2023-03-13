b588ec3d320fd4189de37f9e664b8370c8702786
hotovecj <test@example.com>
2023-03-13 23:44:49 +0100

    feat: add Vertex

---
+++ Czu.WeightedGraph.Core/Vertex.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Czu.WeightedGraph.Core
+{
+    public readonly struct Vertex : IEquatable<Vertex>
+    {
+        public Vertex(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException(nameof(name));
+            }
+
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public override string ToString() => Name;
+
+        public static implicit operator string(Vertex vertex)
+        {
+            return vertex.Name;
+        }
+
+        public bool Equals(Vertex other)
+        {
+            return string.Equals(Name, other.Name, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Vertex other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return StringComparer.OrdinalIgnoreCase.GetHashCode(Name);
+        }
+
+        public static bool operator ==(Vertex left, Vertex right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Vertex left, Vertex right)
+        {
+            return !left.Equals(right);
+        }
+    }
+}
