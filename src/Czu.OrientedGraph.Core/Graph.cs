bdac258d028d52396271036143883ba8f9c15cb9
hotovecj <test@example.com>
2023-03-16 00:51:42 +0100

    feat: add Graph

--- src/Czu.OrientedGraph.Core/Graph.cs
+++ src/Czu.OrientedGraph.Core/Graph.cs
@@ -1,11 +1,38 @@
-﻿namespace Czu.OrientedGraph.Core
-{
-    public sealed class Graph
-    {
-        public void ToSnapshot()
-        {}
+﻿using System.Collections.Generic;
 
-        public void FromSnapshot()
-        {}
+namespace Czu.OrientedGraph.Core
+{
+    public sealed class Graph<T> where T : IEdge
+    {
+        private readonly HashSet<T> _edges = new HashSet<T>();
+
+        public void UpsertEdge(T edge)
+        {
+            TryDeleteEdge(edge);
+
+            _edges.Add(edge);
+        }
+
+        public void TryDeleteEdge(T edge)
+        {
+            if (_edges.Contains(edge))
+            {
+                _edges.Remove(edge);
+            }
+        }
+
+        public SnapshotGraph<T> ToSnapshot() =>
+            new SnapshotGraph<T>(_edges);
+
+        public static Graph<T> FromSnapshot(SnapshotGraph<T> snapshot)
+        {
+            var graph = new Graph<T>();
+            foreach (var edge in snapshot.Edges)
+            {
+                graph.UpsertEdge(edge);
+            }
+
+            return graph;
+        }
     }
 }
---
+++ src/Czu.OrientedGraph.Core/SnapshotGraph.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Czu.OrientedGraph.Core
+{
+    public sealed class SnapshotGraph<T> where T : IEdge
+    {
+        public SnapshotGraph(IEnumerable<T> edges)
+        {
+            Edges = edges ?? throw new ArgumentNullException(nameof(edges));
+        }
+
+        public IEnumerable<T> Edges { get; }
+    }
+}
