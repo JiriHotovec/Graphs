ac46f3f78208f372ff07bf6578c915cbb73f00dd
hotovecj <test@example.com>
2023-03-19 22:10:46 +0100

    feat: add IDataStorage

---
+++ src/Czu.OrientedGraph.Core/IDataStorage.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Czu.OrientedGraph.Core
+{
+    public interface IDataStorage<T> where T : IEdge
+    {
+        Task UpsertAsync(SnapshotGraph<T> snapshot, CancellationToken cancellationToken = default);
+
+        Task<SnapshotGraph<T>> GetAsync(string name, CancellationToken cancellationToken = default);
+
+        Task<bool> ExistsAsync(string name, CancellationToken cancellationToken = default);
+    }
+}
